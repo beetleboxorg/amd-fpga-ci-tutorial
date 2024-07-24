12b0963152ad156e42b70bdecc9232e6188dd47f
AndrewSwirski <andrew.swirski@hotmail.co.uk>
2024-07-24 18:03:47 +0100

    Update run_gen_bitstream.tcl

--- tcl_scripts/run_gen_bitstream.tcl
+++ tcl_scripts/run_gen_bitstream.tcl
@@ -1,7 +1,5 @@
 open_project ci_for_fpgas.xpr
-validate_bd_design
 update_compile_order -fileset sources_1
-set_property top design_1_wrapper [current_fileset]
-update_compile_order -fileset sources_1
+reset_run impl_1 -prev_step 
 launch_runs impl_1 -to_step write_bitstream -jobs 4
 
