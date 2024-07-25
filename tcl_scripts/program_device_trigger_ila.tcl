171b20bdfae698a349f038b338110a6f9ca1f07d
AndrewSwirski <andrew.swirski@hotmail.co.uk>
2024-07-25 12:41:56 +0100

    Update program_device_trigger_ila.tcl

--- tcl_scripts/program_device_trigger_ila.tcl
+++ tcl_scripts/program_device_trigger_ila.tcl
@@ -8,3 +8,4 @@ program_hw_devices [get_hw_devices xc7a100t_0]
 refresh_hw_device [lindex [get_hw_devices xc7a100t_0] 0]
 run_hw_ila [get_hw_ilas -of_objects [get_hw_devices xc7a100t_0] -filter {CELL_NAME=~"design_1_i/ila_0"}]
 wait_on_hw_ila [get_hw_ilas -of_objects [get_hw_devices xc7a100t_0] -filter {CELL_NAME=~"design_1_i/ila_0"}]
+write_hw_ila_data {./iladata.ila} hw_ila_data_1
