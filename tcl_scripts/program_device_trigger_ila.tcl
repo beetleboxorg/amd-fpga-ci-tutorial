16965eaf16a79972a8c7dd2c8155f4ef623c5536
AndrewSwirski <andrew.swirski@hotmail.co.uk>
2024-07-25 12:28:28 +0100

    Update program_device_trigger_ila.tcl

--- tcl_scripts/program_device_trigger_ila.tcl
+++ tcl_scripts/program_device_trigger_ila.tcl
@@ -5,3 +5,6 @@ set_property PROBES.FILE {./ci_for_fpgas.runs/impl_1/design_1_wrapper.ltx} [get_
 set_property FULL_PROBES.FILE {./ci_for_fpgas.runs/impl_1/design_1_wrapper.ltx} [get_hw_devices xc7a100t_0]
 set_property PROGRAM.FILE {./ci_for_fpgas.runs/impl_1/design_1_wrapper.bit} [get_hw_devices xc7a100t_0]
 program_hw_devices [get_hw_devices xc7a100t_0]
+refresh_hw_device [lindex [get_hw_devices xc7a100t_0] 0]
+run_hw_ila [get_hw_ilas -of_objects [get_hw_devices xc7a100t_0] -filter {CELL_NAME=~"design_1_i/ila_0"}]
+wait_on_hw_ila [get_hw_ilas -of_objects [get_hw_devices xc7a100t_0] -filter {CELL_NAME=~"design_1_i/ila_0"}]
